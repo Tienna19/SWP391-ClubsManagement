0f59710ce90d99f8d6d7c089d247fc3d4972d8a5
haivmhe171878-del <haivmhe171878@fpt.edu.vn>
2025-09-29 00:19:51 +0700

    Add function Assign Roles (Leader/Admin)

---
+++ src/java/model/Membership.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Membership {
+    private int membershipId;
+    private int clubId;
+    private int userId;
+    private String role;
+    private String status;
+
+    public Membership() {}
+
+    public Membership(int membershipId, int clubId, int userId, String role, String status) {
+        this.membershipId = membershipId;
+        this.clubId = clubId;
+        this.userId = userId;
+        this.role = role;
+        this.status = status;
+    }
+
+    public int getMembershipId() { 
+        return membershipId; 
+    }
+    public void setMembershipId(int membershipId) { 
+        this.membershipId = membershipId; 
+    }
+
+    public int getClubId() { 
+        return clubId; 
+    }
+    public void setClubId(int clubId) { 
+        this.clubId = clubId; 
+    }
+
+    public int getUserId() { 
+        return userId; 
+    }
+    public void setUserId(int userId) { 
+        this.userId = userId; 
+    }
+
+    public String getRole() { 
+        return role; 
+    }
+    public void setRole(String role) { 
+        this.role = role; 
+    }
+
+    public String getStatus() { 
+        return status; 
+    }
+    public void setStatus(String status) { 
+        this.status = status; 
+    }
+}
